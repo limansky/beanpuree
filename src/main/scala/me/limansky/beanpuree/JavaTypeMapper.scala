6f9b4ce1f5d61ff8378a62af01846cdfd62a9407
Mike Limansky <mike.limansky@gmail.com>
2017-07-30 00:32:55 +0300

    Implement JavaTypeMapper

---
+++ src/main/scala/me/limansky/beanpuree/JavaTypeMapper.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 Mike Limansky
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.limansky.beanpuree
+
+import shapeless.{::, HList, Lazy}
+
+trait JavaTypeMapper[J, S] {
+  def javaToScala(j: J): S
+  def scalaToJava(s: S): J
+}
+
+object JavaTypeMapper {
+  implicit val integerMapper: JavaTypeMapper[Integer, Int] = of(_.intValue, Integer.valueOf)
+
+  implicit val longMapper: JavaTypeMapper[java.lang.Long, Long] = of(_.longValue, java.lang.Long.valueOf)
+
+  implicit val doubleMapper: JavaTypeMapper[java.lang.Double, Double] = of(_.doubleValue, java.lang.Double.valueOf)
+
+  implicit val bigDecimalMapper: JavaTypeMapper[java.math.BigDecimal, BigDecimal] = of(BigDecimal.apply, _.underlying)
+
+  implicit val bigIntegerMapper: JavaTypeMapper[java.math.BigInteger, BigInt] = of(BigInt.apply, _.underlying)
+
+  implicit val charMapper: JavaTypeMapper[Character, Char] = of(_.charValue, Character.valueOf)
+
+  implicit def dummyMapper[T]: JavaTypeMapper[T, T] = of(identity, identity)
+
+  implicit def nullableToMappedOption[J >: Null, S](implicit inner: JavaTypeMapper[J, S]): JavaTypeMapper[J, Option[S]] = of(
+    x => Option(x).map(inner.javaToScala),
+    _.map(inner.scalaToJava).orNull
+  )
+
+  implicit def hconsMapper[JH, JT <: HList, SH, ST <: HList](implicit
+    hMapper: Lazy[JavaTypeMapper[JH, SH]],
+    tMapper: JavaTypeMapper[JT, ST]
+  ): JavaTypeMapper[JH :: JT, SH :: ST] = new JavaTypeMapper[JH :: JT, SH :: ST] {
+    override def javaToScala(j: JH :: JT): SH :: ST = {
+      hMapper.value.javaToScala(j.head) :: tMapper.javaToScala(j.tail)
+    }
+
+    override def scalaToJava(s: SH :: ST): JH :: JT = {
+      hMapper.value.scalaToJava(s.head) :: tMapper.scalaToJava(s.tail)
+    }
+  }
+
+  def of[J, S](jts: J => S, stj: S => J): JavaTypeMapper[J, S] = new JavaTypeMapper[J, S] {
+    override def javaToScala(j: J): S = jts(j)
+    override def scalaToJava(s: S): J = stj(s)
+  }
+}
---
+++ src/test/scala/me/limansky/beanpuree/JavaTypeMapperTest.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2017 Mike Limansky
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.limansky.beanpuree
+
+import java.math.{ BigInteger, BigDecimal => JavaBigDecimal }
+
+import org.scalatest.{FlatSpec, Matchers}
+import shapeless.{::, HNil}
+
+class JavaTypeMapperTest extends FlatSpec with Matchers {
+
+  "Mappers" should "support mapping between Java Integer and Int" in {
+    val m = implicitly[JavaTypeMapper[Integer, Int]]
+
+    m.javaToScala(Integer.valueOf(42)) shouldEqual 42
+    m.scalaToJava(25) shouldEqual Integer.valueOf(25)
+  }
+
+  it should "support mapping between Java Double and Double" in {
+    val m = implicitly[JavaTypeMapper[java.lang.Double, Double]]
+
+    m.javaToScala(java.lang.Double.valueOf(42)) shouldEqual 42d
+    m.scalaToJava(25d) shouldEqual java.lang.Double.valueOf(25)
+  }
+
+  it should "support mapping between Java Long and Long" in {
+    val m = implicitly[JavaTypeMapper[java.lang.Long, Long]]
+
+    m.javaToScala(java.lang.Long.valueOf(42)) shouldEqual 42l
+    m.scalaToJava(25l) shouldEqual java.lang.Long.valueOf(25)
+  }
+
+  it should "support mapping between Character and Char" in {
+    val m = implicitly[JavaTypeMapper[Character, Char]]
+
+    m.javaToScala(Character.valueOf('a')) shouldEqual 'a'
+    m.scalaToJava('b') shouldEqual Character.valueOf('b')
+  }
+
+  it should "support mapping between Java BigDecimal and BigDecimal" in {
+    val m = implicitly[JavaTypeMapper[JavaBigDecimal, BigDecimal]]
+
+    m.javaToScala(JavaBigDecimal.valueOf(42)) shouldEqual BigDecimal(42)
+    m.scalaToJava(1234) shouldEqual JavaBigDecimal.valueOf(1234)
+  }
+
+  it should "support mapping between BigInteger and BigInt" in {
+    val m = implicitly[JavaTypeMapper[BigInteger, BigInt]]
+
+    m.javaToScala(BigInteger.valueOf(223322)) shouldEqual BigInt(223322)
+    m.scalaToJava(4321) shouldEqual BigInteger.valueOf(4321)
+  }
+
+  it should "throw NPE if Java number class is null" in {
+    val m = implicitly[JavaTypeMapper[Integer, Int]]
+    an[NullPointerException] should be thrownBy m.javaToScala(null)
+  }
+
+  it should "throw NPE if Java BigDecimal is null" in {
+    val m = implicitly[JavaTypeMapper[java.math.BigDecimal, BigDecimal]]
+    an[NullPointerException] should be thrownBy m.scalaToJava(null)
+
+  }
+
+  it should "support mapping T to Option[T]" in {
+    val m = implicitly[JavaTypeMapper[String, Option[String]]]
+
+    m.javaToScala("hello") shouldEqual Some("hello")
+    m.scalaToJava(Some("world")) shouldEqual "world"
+
+    m.javaToScala(null) shouldEqual None
+    m.scalaToJava(None) shouldBe null
+  }
+
+  it should "support mapping T to Option[U]" in {
+    val m = implicitly[JavaTypeMapper[Integer, Option[Int]]]
+
+    m.javaToScala(Integer.valueOf(12345)) shouldEqual Some(12345)
+    m.javaToScala(null) shouldEqual None
+
+    m.scalaToJava(Some(54321)) shouldEqual 54321
+    m.scalaToJava(None) shouldBe null
+  }
+
+  it should "support mapping HLists" in {
+    val m = implicitly[JavaTypeMapper[Integer :: String :: java.lang.Double :: HNil, Int :: String :: Option[Double] :: HNil]]
+
+    m.javaToScala(Integer.valueOf(42) :: "test me" :: java.lang.Double.valueOf(33d) :: HNil) shouldEqual 42 :: "test me" :: Some(33d) :: HNil
+    m.javaToScala(Integer.valueOf(142) :: null :: null :: HNil) shouldEqual 142 :: null :: None :: HNil
+
+    m.scalaToJava(212 :: "abc" :: Some(13.33) :: HNil) shouldEqual Integer.valueOf(212) :: "abc" :: 13.33 :: HNil
+    m.scalaToJava(222 :: null :: None :: HNil) shouldEqual Integer.valueOf(222) :: null :: null :: HNil
+  }
+}
